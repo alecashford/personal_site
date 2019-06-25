e7690d29dcaae8397ba0e46e883081a921c626cd
Alec Ashford <ashford91@gmail.com>
2019-06-24 18:00:54 -0700

    built out about page, styling

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ if [ -f secrets.sh ]; then
   . secrets.sh
 fi
 
-pipenv run python manage.py runserver 0.0.0.0:8000
+# Start Gunicorn app server
+pipenv run gunicorn personal_site.wsgi:application \
+               --bind 0.0.0.0:8000 \
+               --workers 5
 
 # Now run the main container CMD, replacing this script.
 exec "$@"
